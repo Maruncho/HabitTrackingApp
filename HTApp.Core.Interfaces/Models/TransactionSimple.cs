46eaca09aef7e69929ead62f48a21ef07a1d8c24
Maruncho <legomarkata@gmail.com>
2024-11-15 20:41:10 +0200

    Add Transaction Repository

---
+++ HTApp.Core.Interfaces/Models/TransactionSimple.cs
@@ -0,0 +1,12 @@
+﻿namespace HTApp.Core.Contracts;
+
+public class TransactionSimple
+{
+    public int Id { get; set; }
+
+    public required string Type { get; set; }
+
+    public required string Message { get; set; }
+
+    public int Amount { get; set; }
+}
---
+++ HTApp.Infrastructure.Repositories/TransactionRepository.cs
@@ -0,0 +1,47 @@
+﻿using HTApp.Core.Contracts;
+using HTApp.Infrastructure.EntityModels;
+using HTApp.Infrastructure.EntityModels.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace HTApp.Infrastructure.Repositories
+{
+    using Transaction = EntityModels.Core.Transaction;
+
+    public class TransactionRepository
+        : GenericRepositoryImmutableBase<Transaction, int>
+        , ITransactionRepository<string, int, Transaction>
+    {
+        private Dictionary<int, string> enumConverter =
+            Enum.GetValues(typeof(TransactionEnum))
+               .Cast<TransactionEnum>()
+               .ToDictionary(t => (int)t, t => t.ToString() );
+
+        public TransactionRepository(ApplicationDbContext db, ILogger logger) : base(db, logger)
+        {
+        }
+
+        public Task<TransactionSimple[]> GetSimpleAll(string userId)
+        {
+            return db.Transactions
+                .Where(t => t.UserId == userId)
+                .Where(t => t.TypeId != null) //It's necessary for the enumConverter above. Shouldn't happen anyway, unless someone(me) screwed up.
+                .Select(t => new TransactionSimple
+                {
+                    Id = t.Id,
+                    Type = enumConverter[t.TypeId!.Value],
+                    Message = t.Type!.Message ?? string.Empty,
+                    Amount = t.Amount
+                })
+                .ToArrayAsync();
+        }
+
+        public Task<string[]> GetTypeNames()
+        {
+            return db.Transactions
+                .Where(t => t.TypeId != null)
+                .Select(t => enumConverter[t.TypeId!.Value])
+                .ToArrayAsync();
+        }
+    }
+}
